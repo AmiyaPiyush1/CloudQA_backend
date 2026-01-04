b5f36101c109a34f9b777c89f7a47651be81fe3b
AmiyaPiyush1 <gamingzone.09990@gmail.com>
2026-01-04 13:37:12 +0530

    updated the model

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '50mb' }));
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash-latest",
+    model: "gemini-1.5-flash-001",
     generationConfig: { 
         responseMimeType: "application/json",
         temperature: 0 
